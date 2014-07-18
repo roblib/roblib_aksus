81ee4c467fffdbe09e4bdd8f4bc555d18f04fac8
Paul Pound <ppound@upei.ca>
2014-07-18 13:59:41 -0300

    initial commit

---
+++ theme/roblib-askus.tpl.php
@@ -0,0 +1,11 @@
+<div><!-- Place this div in your web page where you want your chat widget to appear. -->
+  <div class="needs-js">JavaScript disabled or chat unavailable.</div>
+  <!-- Place this script as near to the end of your BODY as possible. --><script type="text/javascript">
+    (function() {
+      var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+      x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?5343";
+      var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
+    })();
+  </script></div>
+
+<div></div>
